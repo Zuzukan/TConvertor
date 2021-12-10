eb3d4dd3d7820d7bd680a6feed98abf26f007f79
RuslanBihun <bigun.ruslan@gmail.com>
2021-12-10 12:17:54 +0400

    1 Final

--- TConvertor/ViewController.swift
+++ TConvertor/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var celciuseLabel: UILabel!
+    @IBOutlet var fahrenheitLabel: UILabel!
+    
+    @IBOutlet var sliderSetup: UISlider!{
+        didSet {
+            sliderSetup.maximumValue = 100
+            sliderSetup.minimumValue = -100
+            sliderSetup.value = 0
+            sliderSetup.maximumTrackTintColor = .red
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        convert()
     }
 
-
+    @IBAction func sliderAction(_ sender: UISlider) {
+        convert()
+    }
+    
+    func convert() {
+        let celceus = Int(round(sliderSetup.value))
+        celciuseLabel.text = "\(celceus)ºC"
+        
+        let fahrenheit = Int(round(sliderSetup.value * (9 / 5) + 32))
+        fahrenheitLabel.text = "\(fahrenheit)ºF "
+    }
+    
 }
 
